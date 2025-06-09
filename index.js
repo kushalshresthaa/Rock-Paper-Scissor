a783ea57ad2b4e91200610020f9c4e3bd7736450
kushalshresthaa <kushal.sthaa@gmail.com>
2025-06-09 20:04:58 +0545

    updated js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let compScore = 0;
+
 function getComputerChoice(){
     const randomNumber = Math.floor (Math.random()* 3)
 
@@ -9,19 +12,16 @@ function getComputerChoice(){
         return "scissor";
     }
 }
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
 
 function getHumanChoice (){
     const userChoice = prompt("Rock! Paper! Scissor!");
     return userChoice.toLowerCase();
 }
-console.log(getHumanChoice())
-
-let humanScore = 0;
-let compScore = 0;
+//console.log(getHumanChoice())
 
 function playRound(humanChoice, compChoice){ 
-    if (humanChoice === compChoice) {
+        if (humanChoice === compChoice) {
         return "It's a Draw!";
     } else if ((humanChoice === "rock" && compChoice=== "scissor") ||
                 (humanChoice === "scissor" && compChoice === "paper") ||
@@ -39,16 +39,20 @@ function playGame() {
     for (let i=1; i<=5; i++) {
         const humanChoice = getHumanChoice();
         const compChoice = getComputerChoice();
+
+        console.log (`You chose ${humanChoice}`);
+        console.log(`Computer chose ${compChoice}`)
         console.log(playRound(humanChoice, compChoice));
     }
     if (humanScore > compScore){
-        console.log(`You win ${humanScore} by ${compScore} `);
+        console.log(`You win ${humanScore} ${compScore} `);
     }
     else if (humanScore < compScore){
-        console.log(`You loose ${humanScore} by ${compScore} `);
+        console.log(`You lose ${humanScore} ${compScore} `);
     }
     else{
-      console.log(`Draw ${humanScore} by ${compScore}`)
+      console.log(`Draw ${humanScore} ${compScore}`)
     }
 }
 
+playGame()
